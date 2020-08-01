c05bd6d2f7f70caa608469868e0acbcd43e44f71
SenthilKPrakash <senthilaravindh95@gmail.com>
2020-08-01 20:05:53 +0530

    New changes

---
+++ HTML_test/Classes.py
@@ -0,0 +1,4 @@
+class Example():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
---
+++ HTML_test/Hello.py
@@ -0,0 +1,4 @@
+from Classes import Example
+example = Example(2,8)
+print(example.x)
+print(example.y)
